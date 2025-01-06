dcae885201dbd49ac5b5dc95b8685aba2da997ba
Raphael Kagermeier <44846337+raphael-kagermeier@users.noreply.github.com>
2025-01-06 11:01:04 +0100

    add keys to active campaign config

--- config/activecampaign.php
+++ config/activecampaign.php
@@ -1,6 +1,8 @@
 <?php
 
-// config for PerformRomance/ActiveCampaign
 return [
-
+    'api_url' => env('AC_API_URL'),
+    'api_key' => env('AC_API_KEY'),
+    'api_version' => env('AC_API_VERSION', '3'),
+    'default_tags' => []
 ];
